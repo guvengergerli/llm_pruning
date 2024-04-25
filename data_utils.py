e1fa32f5305de9c5e05becdde26fc6d0a03ad561
Gergerlee <guven.gergerli@ug.bilkent.edu.tr>
2024-04-25 00:19:24 -0400

    cola and fix

--- data_utils.py
+++ data_utils.py
@@ -1,50 +1,53 @@
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
-
+"""
 class GLUE_Dataset(Dataset):
     def __init__(self, 
                  data_df,
-                 data_cols, 
-                 eval_col) -> None:
+                 sentence_col, 
+                 label_col) -> None:
         super().__init__()
         self.data_df = data_df
-        self.data_cols = data_cols 
-        self.eval_col = eval_col 
+        self.sentence_col = sentence_col
+        self.label_col = label_col 
 
     def __len__(self):
         return len(self.data_df)
     
     def __getitem__(self, index):
-        if len(self.data_cols) == 1: 
-            return_dict = (
-                self.data_df.iloc[index][self.data_cols[0]],
-                self.data_df.iloc[index][self.eval_col]
-            )
-        else: 
-            return_dict = (
-                self.data_df.iloc[index][self.data_cols[0]],
-                self.data_df.iloc[index][self.data_cols[1]],
-                self.data_df.iloc[index][self.eval_col]
-            )
-        return return_dict
-    
+        # Assuming the sentence is in the fourth column and the label is in the second column
+        sentence = self.data_df.iloc[index, self.sentence_col]
+        label = self.data_df.iloc[index, self.label_col]
+        return sentence, label
+"""
 
-def get_data_df(data_dir, task, train=False):
-    if train: 
-        task = f"{data_dir}/{task}/train.tsv"
-    else: 
-        task = f"{data_dir}/{task}/test.tsv"
-    h=None
-    if task == 'CoLA':
-        h = 0
 
-    df = pd.read_csv(task, sep='\t', header=h)
+def get_data_df(data_dir, task, train_int): # 0 train 1 dev 2 test
+    filename = 'train.tsv' if train_int == 0 else 'dev.tsv' if train_int == 1 else 'test.tsv'
+    task_path = f"{data_dir}/{task}/{filename}"
+    header = 0 if task == 'CoLA' else None
+
+    df = pd.read_csv(task_path, sep='\t', header=header)
     return df
 
 
-def construct_dataset(data_df): 
-    return 
+def construct_dataset(data_df, sentence_col, label_col):
+    return GLUE_Dataset(data_df=data_df, sentence_col=sentence_col, label_col=label_col)
 
+class GLUE_Dataset(Dataset):
+    def __init__(self, 
+                 data_df,
+                 sentence_col,
+                 label_col):
+        super().__init__()
+        self.data_df = data_df
+        self.sentence_col = sentence_col
+        self.label_col = label_col 
 
-
-
+    def __len__(self):
+        return len(self.data_df)
+    
+    def __getitem__(self, index):
+        sentence = self.data_df.iloc[index, self.sentence_col]
+        label = self.data_df.iloc[index, self.label_col]
+        return sentence, label
